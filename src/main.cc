411a8fe0408155efd4fe74f431c2f6d6c8c33aee
mygosity <mygosity@gmail.com>
2020-10-15 09:01:01 +1100

    wip2

--- src/main.cc
+++ src/main.cc
@@ -42,50 +42,14 @@ static std::array<std::array<int, 5001>, 5001> dp;
 
 int commonChild(string& s1, string& s2, int i = 0, int j = 0) {
     if (i == 0 && j == 0) {
-        for (auto& itr : dp)
-            for (auto& itry : itr) itry = -1;
-
-        // letters1.clear();
+        for (int i = 0; i < dp.size(); ++i) {
+            for (int j = 0; j < dp[i].size(); ++j) {
+                dp[i][j] = 0;
+            }
+        }
     }
-    if (i >= s1.size() - 1 || j >= s2.size() - 1) return 0;
     int total = 0;
 
-    vector<int> letters1(26, -1);
-    for (; i < s1.size(); ++i) {
-        const int c = (int)s1[i] - (int)'A';
-        if (letters1[c] == -1) {
-            letters1[c] = i;
-        }
-    }
-    vector<int> letters2(26, -1);
-    int max = 0;
-    int earliestIndex = 15001;
-
-    for (; j < s2.size(); ++j) {
-        const int c = (int)s2[j] - (int)'A';
-        const int targetIndex = letters1[c];
-        if (targetIndex > -1 && targetIndex < earliestIndex && letters2[c] == -1) {
-            earliestIndex = targetIndex;
-
-            int currentLength = 1;
-            letters2[c] = targetIndex;
-            if (targetIndex + 1 < 5000 && j + 1 < 5000) {
-                int v = dp[targetIndex + 1][j + 1];
-                if (v == -1) {
-                    int value = commonChild(s1, s2, targetIndex + 1, j + 1);
-                    dp[targetIndex + 1][j + 1] = value;
-                    currentLength += value;
-                } else {
-                    currentLength += v;
-                }
-                if (currentLength > max) {
-                    max = currentLength;
-                }
-            }
-            if (earliestIndex == i) break;
-        }
-    }
-    total += max;
     return total;
 }
 
@@ -138,15 +102,15 @@ int main(int argc, char* argv[]) {
 
     const std::string customFilePath = "./inputs/hacker_rank/interview_prep_kit/strings/commonchild/";
     const std::vector<std::vector<std::string>> cases({
-        {customFilePath + "sample01.txt", "2"},  //
-        {customFilePath + "sample02.txt", "0"},  //
-        {customFilePath + "sample03.txt", "3"},  //
-        {customFilePath + "input01.txt", "15"},  //
-        {customFilePath + "input02.txt", "27"},  //
-        // {customFilePath + "input04.txt", "321"},   //
-        // {customFilePath + "input05.txt", "1417"},  //
-        // {customFilePath + "input10.txt", "1618"},  //
-        // {customFilePath + "input13.txt", "1618"},  //
+        {customFilePath + "sample01.txt", "2"},    //
+        {customFilePath + "sample02.txt", "0"},    //
+        {customFilePath + "sample03.txt", "3"},    //
+        {customFilePath + "input01.txt", "15"},    //
+        {customFilePath + "input02.txt", "27"},    //
+        {customFilePath + "input04.txt", "321"},   //
+        {customFilePath + "input05.txt", "1417"},  //
+        {customFilePath + "input10.txt", "1618"},  //
+        {customFilePath + "input13.txt", "1618"},  //
     });
 
     for (const std::vector<std::string>& vs : cases) {
